17e7d0286b338bcb4ab203920cdd17fdd32fdf9a
sushant0789 <53333023+sushant0789@users.noreply.github.com>
2020-10-29 18:51:19 +0530

    new page

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,3 @@
-$(".select-box select").change(function () {
-  var theText = $(this).val();
-  $(this).removeClass().addClass(theText);
-});
-
 $(document).ready(function () {
   // Add minus icon for collapse element which is open by default
   $(".collapse.show").each(function () {
@@ -29,4 +24,15 @@ $(document).ready(function () {
         .removeClass("fa-angle-up")
         .addClass("fa-angle-down");
     });
+});
+
+// Tooltips Initialization
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+// Select Box
+$(".select-box select").change(function () {
+  var theText = $(this).val();
+  $(this).removeClass().addClass(theText);
 });
