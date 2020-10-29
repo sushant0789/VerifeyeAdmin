3b6105ffe57c32108d48410f9e85170c463f3a5b
sushant0789 <53333023+sushant0789@users.noreply.github.com>
2020-10-29 12:58:28 +0530

    few changes

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,32 @@
-$('.select-box select').change(function () {
-    var theText = $(this).val();
-    $(this).removeClass().addClass(theText);
+$(".select-box select").change(function () {
+  var theText = $(this).val();
+  $(this).removeClass().addClass(theText);
+});
+
+$(document).ready(function () {
+  // Add minus icon for collapse element which is open by default
+  $(".collapse.show").each(function () {
+    $(this)
+      .prev(".card-header")
+      .find(".fa")
+      .addClass("fa-angle-up")
+      .removeClass("fa-angle-down");
+  });
+
+  // Toggle plus minus icon on show hide of collapse element
+  $(".collapse")
+    .on("show.bs.collapse", function () {
+      $(this)
+        .prev(".card-header")
+        .find(".fa")
+        .removeClass("fa-angle-down")
+        .addClass("fa-angle-up");
+    })
+    .on("hide.bs.collapse", function () {
+      $(this)
+        .prev(".card-header")
+        .find(".fa")
+        .removeClass("fa-angle-up")
+        .addClass("fa-angle-down");
+    });
 });
